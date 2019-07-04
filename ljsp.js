a32901bbfee42af0a6c5a33815cbf5ae183c45cf
tr3nx <0wnedbytr3nx@gmail.com>
2019-07-04 08:34:29 -0700

    epiphany while parsing

--- ljsp.js
+++ ljsp.js
@@ -1,3 +1,12 @@
+// Lisp expression model
+// expr
+// - atomic
+// -- self-evalulating
+// -- symbols (variables)
+// - list
+// -- procedure calls
+// -- special forms
+
 let InputReader = (input) => {
 	let pos = 0;
 	return {
@@ -32,7 +41,13 @@ let Tokenizer = (reader, types) => {
 			let match = code.match(new RegExp(`^(${Object.values(type)[0]})`));
 			if (match === null || match.length <= 1) return;
 			reader.skip(match[match.length - 1].length);
-			tokens.push({ type: Object.keys(type)[0], value: match[match.length - 1] });
+			
+			type = Object.keys(type)[0];
+			
+			let value = match[match.length - 1];
+			if (type === "integer") value = Number(value);
+
+			tokens.push({ type, value });
 			return true;
 		});
 	}
@@ -42,58 +57,116 @@ let Tokenizer = (reader, types) => {
 let Parser = (tokens) => {
 	return {
 		parse: function() {
-			if (this.peek("oparen")) {
-				return this.parse_expr();
-			}
-
-			if (this.peek("integer")) {
-				return Number(this.consume("integer").value);
-			}
-
-			if (this.peek("string")) {
-				return String(this.consume("string").value);
-			}
-
-			return this.consume("symbol").value;
+			return this.parse_expr();
 		},
 		parse_expr: function() {
-			this.consume("oparen");
-			if (this.peek("oparen")) {
-				return this.parse();
+			if (this.peek() === "oparen") {
+				return this.parse_list();
 			}
 
-			let proc = {
-				name : this.consume("symbol").value,
-				args : this.parse_args()
-			};
+			return this.parse_atomic();
+		},
+		parse_atomic: function() {
+			// self-evaluating
+			if (this.peek() === "integer") {
+				return this.consume("integer").value;
+			}
+
+			if (this.peek() === "string") {
+				return this.consume("string").value;
+			}
+
+			// symbols/variables
+			return this.consume("symbol").value;
+		},
+		parse_list: function() {
+			if (this.peek(1) === "symbol") {
+				if (tokens[1].value === "lambda") {
+					return this.parse_lambda();
+				}
+
+				if (tokens[1].value === "quote") {
+					return this.parse_quote();
+				}
+			}
+
+			return this.parse_procedure();
+		},
+		parse_lambda: function() {
+			this.consume("oparen");
+			this.consume("symbol");
+			this.consume("oparen");
+
+			let argvars = [];
+			while (this.peek() !== "cparen") {
+				argvars.push(this.consume("symbol").value);
+			}
 
 			this.consume("cparen");
 
-			return proc;
+			let body = this.parse_expr();
+
+			this.consume("cparen");
+
+			return { type: "lambda", argvars: argvars, body: body };
 		},
-		parse_args : function() {
-			let args = [];
-			while ( ! this.peek("cparen")) {
-				args.push(this.parse());
+		parse_procedure: function() {
+			this.consume("oparen");
+
+			let rator;
+			if (this.peek() === "oparen") {
+				rator = this.parse_list();
+			} else {
+				rator = this.consume("symbol").value;
 			}
-			return args;
+
+			let rand = [];
+			while (this.peek() !== "cparen") {
+				rand.push(this.parse_expr());
+			}
+
+			this.consume("cparen");
+
+			return { type: "procedure", func: rator, args: rand };
 		},
-		peek: function(expected, offset=0) {
-			return tokens[offset].type === expected;
+		parse_quote: function() {
+			this.consume("oparen");
+			this.consume("symbol");
+
+			let quoted = [];
+			let depth = 0;
+
+			while (true) {
+				if (this.peek() === "cparen" && depth === 0) break;
+				if (this.peek() === "oparen") depth++;
+				if (this.peek() === "cparen") depth--;
+				quoted.push(tokens.shift().value);
+			}
+
+			this.consume("cparen");
+
+			return quoted.join(" ")
+					  .replace(new RegExp("\\(\\s"), '(')
+					  .replace(new RegExp("\\s\\)"), ')');
+		},
+		peek: function(offset=0) {
+			return tokens[offset].type;
 		},
 		consume: function(expected) {
 			let token = tokens.shift();
 			if (token === undefined || token.type !== expected) {
-				throw(`Syntax Error: Expected '${expected}' but got '${token.type}'.`);
+				throw(`Syntax Error: [${tokens.length}] Expected '${expected}' but got '${token.type}'.`);
 			}
 			return token;
 		}
 	};
 };
 
-const code = '((lambda (x) x) 6)';
 // const code = "(+ 12 18)";
-// const code = "((lambda (x y) (% x y)) 5 35)";
+// const code = '(* 3 (+ 5 6))';
+// const code = "(list (quote +) (quote (1 2 3)))";
+// const code = '(loop 5 (print "testing"))';
+const code = "(abs ((lambda (x) (- 2 (+ 3 x))) (+ 1 (* 4 (* 2 (- (* 5 2) 5))))))";
 console.log("Raw:", code);
 
 const reader = InputReader(code);
@@ -107,15 +180,6 @@ const tokens = Tokenizer(reader, [
 	{ "symbol"  : '[a-zA-Z0-9+=!^%*-/]+' }
 ]);
 console.log("Tokens:", tokens);
-tokens.forEach(t => console.log(t));
 
 const tree = Parser(tokens).parse();
 console.log("Parser:", tree);
-
-// const gen = Generator.new(tree).generate();
-// console.log("Generated:", gen);
-// console.log("Starting: ", code);
-// console.log("Matching:", gen === code);
-
-// const result = Evaluator.new(tree).run();
-// console.log("Eval:", result);
