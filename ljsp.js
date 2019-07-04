9511d470fe1cd38ef959c4f031f7b3a270deb11c
tr3nx <0wnedbytr3nx@gmail.com>
2019-07-04 15:12:00 -0700

    some evaluation

--- ljsp.js
+++ ljsp.js
@@ -1,11 +1,4 @@
-// Lisp expression model
-// expr
-// - atomic
-// -- self-evalulating
-// -- symbols (variables)
-// - list
-// -- special forms
-// -- procedure calls
+// ljsp
 
 let InputReader = (input) => {
 	let pos = 0;
@@ -41,11 +34,13 @@ let Tokenizer = (reader, types) => {
 			let match = code.match(new RegExp(`^(${Object.values(type)[0]})`));
 			if (match === null || match.length <= 1) return;
 			reader.skip(match[match.length - 1].length);
-			
+
 			type = Object.keys(type)[0];
-			
+
 			let value = match[match.length - 1];
-			if (type === "integer") value = Number(value);
+			if (type === "integer") {
+				value = Number(parseFloat(value));
+			}
 
 			tokens.push({ type, value });
 			return true;
@@ -145,6 +140,7 @@ let Parser = (tokens) => {
 			return { type: "quote", quoted: quoted };
 		},
 		peek: function(offset=0) {
+			if (tokens[offset] === undefined) { throw(`Syntax Error: Closing paren missing?`); }
 			return tokens[offset].type;
 		},
 		consume: function(expected) {
@@ -212,13 +208,69 @@ let Generator = (tree) => {
 	};
 };
 
+let Evaluator = (tree) => {
+	return {
+		evaluate: function() {
+			return this._evaluate(tree);
+		},
+		_evaluate: function(node) {
+			if (node.type !== undefined) {
+				if (node.type === "procedure") {
+					return this.evaluate_procedure(node);
+				}
+				if (node.type === "lambda") {
+					return this.evaluate_lambda(node);
+				}
+				if (node.type === "quote") {
+					return this.evaluate_quote(node);
+				}
+			}
+			return node;
+		},
+		evaluate_procedure: function(node) {
+			let args = [];
+			for (let i = 0; i  < node.args.length; i++) {
+				let arg = node.args[i];
+				if (arg instanceof Object) {
+					args.push(this._evaluate(arg));
+				} else {
+					args.push(arg);
+				}
+			}
+			return this[this._evaluate(node.func)](args);
+		},
+		evaluate_quote: function(node) {
+			return node.quoted;
+		},
+		// builtins
+		loop: function(args) {
+			let times = args.shift();
+			let body = args.shift();
+			let ret = "";
+			for (let i = 0; i < times; i++) {
+				ret += this._evaluate(body);
+			}
+			return ret;
+		},
+		print   : (str) => str.join(),
+		abs     : (int) => Math.abs(int),
+		expt    : (args) => args.reduce((a, b) => Math.pow(a, b)),
+		'+'     : (args) => args.reduce((a, b) => a += b),
+		'-'     : (args) => args.reduce((a, b) => a -= b),
+		'*'     : (args) => args.reduce((a, b) => a *= b),
+		'/'     : (args) => args.reduce((a, b) => a /= b),
+		'%'     : (args) => args.reduce((a, b) => a %  b),
+	};
+};
+
 // const code = "42";
 // const code = '"testing"';
-// const code = "(+ 12 (* 2 18) 5)";
-// const code = '(loop 5 (print "testing"))';
-// const code = "(list (quote +) (quote test))";
+const code = "(abs (- 61 (+ 12 (* 2 18) (+ 50 5))))";
+// const code = '(loop (+ (/ 12 4) (* 2 (- (% 5 3) 1))) (print "testing"))';
+// const code = "(quote +)";
+// const code = "(+ 1.1 5.1)";
 // const code = "(abs ((lambda (x) (- 2 (+ 3 x))) (+ 1 (* 4 (* 2 (- (* 5 2) 5))))))";
-const code = '(loop (abs ((lambda (x) (+ x 10)) (quote 32))) (print "hello ljsp"))';
+// const code = '(loop (abs ((lambda (x) (+ x 10)) (quote 32))) (print "hello ljsp"))';
 console.log("Raw:", code);
 
 const reader = InputReader(code);
@@ -227,16 +279,20 @@ console.log("Input:", reader);
 const tokens = Tokenizer(reader, [
 	{ "oparen"  : '\\(' },
 	{ "cparen"  : '\\)' },
-	{ "integer" : '[0-9]+' },
+	{ "integer" : '[\-]?[0-9]+(?:[\.]?[0-9]+)?' },
 	{ "string"  : '\\"[^\"]*\\"' },
 	{ "symbol"  : '[a-zA-Z0-9+=!^%*-/]+' }
 ]);
 console.log("Tokens:", tokens);
+tokens.forEach(t => console.log(t));
 
 const tree = Parser(tokens).parse();
 console.log("Parser:", tree);
 
 const generated = Generator(tree).generate();
 console.log("Generated:", generated);
-console.log("Original: ", code);
-console.log("Matching: ", code === generated);
+console.log("Original :", code);
+console.log("Matching:", code === generated);
+
+const evaluated = Evaluator(tree).evaluate();
+console.log("Evaluated:", evaluated);
