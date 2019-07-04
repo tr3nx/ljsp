0958e775a794ef3e36dbce45243f4aeac81fe1c0
tr3nx <0wnedbytr3nx@gmail.com>
2019-07-04 11:49:38 -0700

    code generator

--- ljsp.js
+++ ljsp.js
@@ -4,8 +4,8 @@
 // -- self-evalulating
 // -- symbols (variables)
 // - list
-// -- procedure calls
 // -- special forms
+// -- procedure calls
 
 let InputReader = (input) => {
 	let pos = 0;
@@ -63,20 +63,15 @@ let Parser = (tokens) => {
 			if (this.peek() === "oparen") {
 				return this.parse_list();
 			}
-
 			return this.parse_atomic();
 		},
 		parse_atomic: function() {
-			// self-evaluating
 			if (this.peek() === "integer") {
 				return this.consume("integer").value;
 			}
-
 			if (this.peek() === "string") {
 				return this.consume("string").value;
 			}
-
-			// symbols/variables
 			return this.consume("symbol").value;
 		},
 		parse_list: function() {
@@ -84,12 +79,10 @@ let Parser = (tokens) => {
 				if (tokens[1].value === "lambda") {
 					return this.parse_lambda();
 				}
-
 				if (tokens[1].value === "quote") {
 					return this.parse_quote();
 				}
 			}
-
 			return this.parse_procedure();
 		},
 		parse_lambda: function() {
@@ -145,9 +138,11 @@ let Parser = (tokens) => {
 
 			this.consume("cparen");
 
-			return quoted.join(" ")
-					  .replace(new RegExp("\\(\\s"), '(')
-					  .replace(new RegExp("\\s\\)"), ')');
+			quoted = quoted.join(" ")
+						.replace(new RegExp("\\(\\s"), '(')
+						.replace(new RegExp("\\s\\)"), ')');
+
+			return { type: "quote", quoted: quoted };
 		},
 		peek: function(offset=0) {
 			return tokens[offset].type;
@@ -162,11 +157,68 @@ let Parser = (tokens) => {
 	};
 };
 
-// const code = "(+ 12 18)";
-// const code = '(* 3 (+ 5 6))';
-// const code = "(list (quote +) (quote (1 2 3)))";
+let Generator = (tree) => {
+	return {
+		generate: function() {
+			return this._generate(tree);
+		},
+		_generate: function(tree) {
+			let code = "";
+
+			if (tree.type !== undefined) {
+				let type = tree.type;
+				if (type === "procedure") {
+					code += this.generate_procedure(tree.func, tree.args);
+				} else if (type === "quote") {
+					code += this.generate_quote(tree.quoted);
+				} else {
+					code += this.generate_lambda(tree.argvars, tree.body);
+				}
+			} else if (tree instanceof Array) {
+				code += tree.map(String);
+			} else {
+				code += tree;
+			}
+
+			return code;
+		},
+		generate_procedure: function(func, args) {
+			let _args = [];
+
+			for (let i = 0; i < args.length; i++) {
+				let arg = args[i];
+				if (arg.type !== undefined) {
+					_args.push(this._generate(args[i]));
+				} else {
+					_args.push(args[i]);
+				}
+			}
+
+			let proc = _args.map(String).join(" ");
+
+			if (func instanceof Object) {
+				func = this._generate(func);
+			}
+ 
+			return "(" + func + " " + proc + ")";
+		},
+		generate_lambda: function(argvars, body) {
+			// (lambda (x) (- 2 (+ 3 x)))
+			return "(lambda (" + argvars.map(String) + ") " + this._generate(body) + ")";
+		},
+		generate_quote: function(quoted) {
+			return "(quote " + quoted + ")";
+		}
+	};
+};
+
+// const code = "42";
+// const code = '"testing"';
+// const code = "(+ 12 (* 2 18) 5)";
 // const code = '(loop 5 (print "testing"))';
-const code = "(abs ((lambda (x) (- 2 (+ 3 x))) (+ 1 (* 4 (* 2 (- (* 5 2) 5))))))";
+// const code = "(list (quote +) (quote test))";
+// const code = "(abs ((lambda (x) (- 2 (+ 3 x))) (+ 1 (* 4 (* 2 (- (* 5 2) 5))))))";
+const code = '(loop (abs ((lambda (x) (+ x 10)) (quote 32))) (print "hello ljsp"))';
 console.log("Raw:", code);
 
 const reader = InputReader(code);
@@ -183,3 +235,8 @@ console.log("Tokens:", tokens);
 
 const tree = Parser(tokens).parse();
 console.log("Parser:", tree);
+
+const generated = Generator(tree).generate();
+console.log("Generated:", generated);
+console.log("Original: ", code);
+console.log("Matching: ", code === generated);
